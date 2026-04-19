fc712c77e14979ba0ac860876ff735e23621e795
nayanrdeveloper <nayanrdeveloper@gmail.com>
2026-04-19 20:08:49 +0530

    Eslint and pretier setup and format the code

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,19 @@ const expoConfig = require('eslint-config-expo/flat');
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ['dist/*', '.expo/*'],
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+
+      'react-native/no-inline-styles': 'warn',
+    },
   },
 ]);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
+  content: ['./App.tsx', './components/**/*.{js,jsx,ts,tsx}'],
+  presets: [require('nativewind/preset')],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
